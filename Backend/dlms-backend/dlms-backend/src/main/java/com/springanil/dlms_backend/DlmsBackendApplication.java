5093449319c1022b3768a22d2a5306bb8e4912f0
Anilkollu <kolluanil555@gmail.com>
2025-05-12 01:05:58 -0400

    dlms-backend updated

---
+++ Backend/dlms-backend/dlms-backend/src/main/java/com/springanil/dlms_backend/DlmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.springanil.dlms_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlmsBackendApplication.class, args);
+	}
+
+}
