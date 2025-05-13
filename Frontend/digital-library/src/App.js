0d0fcc6eb0b23b1f0ff7227673f731fbdff2623b
SaiNiranjanChitturi <147737575+SaiNiranjanChitturi@users.noreply.github.com>
2025-05-12 23:48:33 -0400

    Update App.js

--- Frontend/digital-library/src/App.js
+++ Frontend/digital-library/src/App.js
@@ -1,10 +1,18 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
+
 function App() {
-  return (
+  useEffect(() => {
     axios.get('/api/helloworld')
       .then((result) => console.log(result.data))
-      .catch(console.error)
+      .catch(console.error);
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Check the console for the response from the API!</h1>
+    </div>
   );
 }
 
