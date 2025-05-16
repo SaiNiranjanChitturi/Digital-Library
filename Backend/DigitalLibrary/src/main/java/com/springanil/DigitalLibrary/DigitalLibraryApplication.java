510c17dece4bd05ec30f514558657abc843479a7
Anilkollu <kolluanil555@gmail.com>
2025-05-16 01:38:21 -0400

    .

--- Backend/DigitalLibrary/src/main/java/com/springanil/DigitalLibrary/DigitalLibraryApplication.java
+++ Backend/DigitalLibrary/src/main/java/com/springanil/DigitalLibrary/DigitalLibraryApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.springanil.DigitalLibrary")
-@EnableJpaRepositories(basePackages = "com.springanil.DigitalLibrary.repository")
-
+//@EnableJpaRepositories(basePackages = "com.springanil.DigitalLibrary.repository")
 public class DigitalLibraryApplication {
 
 	public static void main(String[] args) {
