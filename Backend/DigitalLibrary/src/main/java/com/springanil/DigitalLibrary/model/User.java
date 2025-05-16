f52b04b57851e003ebc85e23d972f59ffb814a8c
Anilkollu <kolluanil555@gmail.com>
2025-05-16 16:42:47 -0400

    .

--- Backend/DigitalLibrary/src/main/java/com/springanil/DigitalLibrary/model/User.java
+++ Backend/DigitalLibrary/src/main/java/com/springanil/DigitalLibrary/model/User.java
@@ -2,64 +2,23 @@ package com.springanil.DigitalLibrary.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "users")
+@Data
+@Table(name = "Users")
 public class User {
 
     @Id
+    @Column(name="ID")
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true, length = 45)
+    @Column(name ="USERNAME", nullable = false, unique = true, length = 45)
     private String email;
-    @Column(nullable = false, length = 45)
+
+    @Column(name= "PASSWORD", nullable = false, length = 45)
     private String password;
-    @Column(nullable = false, length =20 )
-    private String firstname;
-    @Column(nullable = false, length = 20)
-    private String lastname;
 
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
