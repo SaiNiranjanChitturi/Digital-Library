268062d1ceba28d616d44f966b4f96d67305cddc
Anilkollu <kolluanil555@gmail.com>
2025-05-15 20:23:58 -0400

    .

--- Backend/DigitalLibrary/src/main/java/com/springanil/DigitalLibrary/controller/UserController.java
+++ Backend/DigitalLibrary/src/main/java/com/springanil/DigitalLibrary/controller/UserController.java
@@ -10,19 +10,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController@RequestMapping("/users")
+@RestController
+@RequestMapping("/api")
 public class UserController {
 
     @Autowired
     private Userservice service;
 
-    @RequestMapping("/")
+    @Autowired
+    public UserController(Userservice service) {
+        this.service = service;
+    }
+
+
+    @GetMapping("/")
     public String greet(){
         return "Hello Dheeraj, Welcome to Digital Library";
     }
 
     @GetMapping("/users")
-    public List<User> getAllProducts(){
-        return service.getAllProducts();
+    public List<User> getAllUsers(){
+        return null;
     }
 }
