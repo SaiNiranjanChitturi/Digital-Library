f558a3b37e3bafce0ffdc964959a8095d16901e0
Anilkollu <kolluanil555@gmail.com>
2025-05-31 14:13:13 -0400

    .

--- Backend/DigitalLibrary/src/main/java/com/springanil/DigitalLibrary/config/SecurityConfig.java
+++ Backend/DigitalLibrary/src/main/java/com/springanil/DigitalLibrary/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**", "/hello", "/api/login").permitAll()
+                        .requestMatchers("/api/auth/**", "/hello", "/api/login", "/api/register").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
