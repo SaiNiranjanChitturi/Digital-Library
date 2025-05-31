bcc3192efce51c4da1e9b029527e854d63716c3d
Anilkollu <kolluanil555@gmail.com>
2025-05-31 02:06:27 -0400

    unwanted imports deleted

--- Backend/DigitalLibrary/src/main/java/com/springanil/DigitalLibrary/config/JwtFilter.java
+++ Backend/DigitalLibrary/src/main/java/com/springanil/DigitalLibrary/config/JwtFilter.java
@@ -3,20 +3,15 @@ package com.springanil.DigitalLibrary.config;
 import com.springanil.DigitalLibrary.model.Users;
 import com.springanil.DigitalLibrary.repository.UsersRepository;
 import com.springanil.DigitalLibrary.service.JwtService;
-import com.springanil.DigitalLibrary.service.UserDetailsServiceImpl;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.context.ApplicationContext;
 
 import java.io.IOException;
 import java.util.ArrayList;
