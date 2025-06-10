ec3ef743776606ca7bc1a61943fd9d67dd4fa683
Anilkollu <kolluanil555@gmail.com>
2025-06-09 21:26:24 -0400

    Update Book.java

--- Backend/DigitalLibrary/src/main/java/com/springanil/DigitalLibrary/model/Book.java
+++ Backend/DigitalLibrary/src/main/java/com/springanil/DigitalLibrary/model/Book.java
@@ -11,23 +11,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "book")
+
+
 public class Book {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private int id;
 
-    @JsonProperty("description")
-    @Column(name = "description")
-    private String description;
-
-    @JsonProperty("image_url")
-    @Column(name = "image_url")
-    private String imageUrl;
-
-    @JsonProperty("name")
-    @Column(name = "name")
-    private String name;
+    @JsonProperty("title")
+    @Column(name = "title")
+    private String title;
 
     @JsonProperty("author")
     @Column(name = "author")
@@ -37,17 +30,24 @@ public class Book {
     @Column(name = "category")
     private String category;
 
-    @JsonProperty("rating")
-    @Column(name = "rating")
-    private int rating;
-
     @JsonProperty("price")
     @Column(name = "price")
     private double price;
 
-    @JsonProperty("sale")
-    @Column(name = "sale")
-    private int sale;
+    @JsonProperty("rating")
+    @Column(name = "rating")
+    private double rating;
 
+    @JsonProperty("sale_percentage")
+    @Column(name = "sale_percentage")
+    private double sale;
+
+    @JsonProperty("description")
+    @Column(name = "description")
+    private String description;
+
+    @JsonProperty("image_url")
+    @Column(name = "image_url")
+    private String imageUrl;
 
 }
